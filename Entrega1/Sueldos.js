83c4411def8e45239efa3fa47dec7150f0bd4cab
Leo Moreno <leomorefer30@gmail.com>
2025-11-21 14:41:41 +0100

    fg

--- Entrega1/Sueldos.js
+++ Entrega1/Sueldos.js
@@ -1,19 +1,24 @@
 let prompt = require('prompt-sync')();
 let num = Number(prompt('Introduce el número de trabajadores: '));
 let suma = 0;
+let maximo=-Infinity;
+let minimo=Infinity;
+let media;
+ let sumatotal = 0;
+ let i=0;
 
-for (let i =1; i<=num; i++) {
+for (i =1; i<=num; i++) {
     let turno = (prompt('Introduce el turno del ' +i+ ' trabajador: '));
-      if (turno != 'primero' || turno != 'segundo' || turno != 'tercero') {
+     /* if (turno != 'primero' || turno != 'segundo' || turno != 'tercero') {
         console.log ('Ese turno no existe.')
         break;
-    }
+    }*/
     let puesto = (prompt('Introduce el puesto del '+i+' trabajador: '));
-     if (puesto != 'obrero' || puesto != 'licenciado' || puesto != 'tecnico') {
+     /*if (puesto != 'obrero' || puesto != 'licenciado' || puesto != 'tecnico') {
         console.log ('Ese puesto no existe.')
         break;
     }
-
+*/
     let horas = (prompt('Introduce el número de horas trabajadas a la semana: '))
 
 
@@ -27,15 +32,28 @@ for (let i =1; i<=num; i++) {
         suma += 300;
     }
      if (puesto == 'obrero') {
-        suma += (30*horas)
+        suma += (30*horas);
      }
      if (puesto == 'tecnico') {
         suma += (50*horas);
     }
      if (puesto == 'licenciado') {
-        suma += (100*horas)
+        suma += (100*horas);
      }
-     let sumatotal = 0;
      sumatotal += suma;
-     console.log('El sueldo total semanal es de: ' +sumatotal);
+     console.log('El sueldo total semanal es de: ' +suma);
+
+     if (maximo<suma) {
+    maximo=suma;
+     }
+
+    if (suma<minimo) {
+    minimo=suma;
+     }
 }
+
+
+
+console.log('El sueldo máximo es de: '+maximo);
+console.log('El sueldo mínimo es de: '+minimo);
+console.log('La media de los sueldos es: '+(sumatotal/(i-1)));
