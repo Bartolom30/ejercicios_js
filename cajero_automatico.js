0b584c5fdd02bc448537e8ccf8c2b96a4b16849e
Leo Moreno <leomorefer30@gmail.com>
2025-11-26 14:20:23 +0100

    njklñhi

--- cajero_automatico.js
+++ cajero_automatico.js
@@ -1,6 +1,7 @@
 //Escribir un programa modular que simule un cajero automático con las opciones de depósitos, retiros y ver saldo.
 
 let prompt = require('prompt-sync')();
+let opcion = Number(prompt('Introduzca la opción que desee: '));
 
 function leerOpcionMenu() {
 
@@ -9,25 +10,26 @@ console.log('Introduce 2 si desea ingresar dinero.');
 console.log('Introduce 3 si desea retirar dinero.');
 console.log('Introduce 4 si desea abandonar el sistema.');
 
-let opcion = Number(prompt('Introduzca la opción que desee: '));
+
 
 if (opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4) {
     console.log('Esa operación no es válida');
 }
 
 if (opcion==4) {
+    console.log('Abandonando el sistema...');
     process.exit();
 }
 console.log('Has elegido la opcion '+opcion);
 }
 
 function leerCantidadPositiva() {
-let dinero;
-    while (dinero<0) {
-    let dinero = Number(prompt('Introduzca una cantidad de dinero: '));
+let num2;
+    while (num2<0) {
+    let num2 = Number(prompt('Introduzca una cantidad de dinero: '));
     console.log('Valor incorrecto');
-    if (dinero>0) {
-        console.log('Has introducido '+dinero);
+    if (num2>0) {
+        console.log('Has introducido '+num2);
         break;
     }
 
@@ -39,7 +41,7 @@ do {
 
     let num=leerOpcionMenu();
 
-    switch (num) {
+    switch (opcion) {
         case 1: 
 
             let num2=leerCantidadPositiva();
